846320d6f473ee910f42b284a84105ae45b535e9
Mauro Servienti <mauro.servienti@gmail.com>
2019-04-23 09:08:32 +0200

    first cry :-)

---
+++ src/ServiceComposer.AspNetCore.Mvc/IHandleResult.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Threading.Tasks;
+
+namespace ServiceComposer.AspNetCore.Mvc
+{
+    public interface IHandleResult : IInterceptRoutes
+    {
+        Task Handle(string requestId, ResultExecutingContext context, dynamic viewModel, int httpStatusCode);
+    }
+}
