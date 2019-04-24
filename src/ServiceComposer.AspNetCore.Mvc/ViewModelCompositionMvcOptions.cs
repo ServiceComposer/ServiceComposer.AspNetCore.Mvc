2f0d8291773e0189e7ceea5342ee24250caef0c4
Mauro Servienti <mauro.servienti@gmail.com>
2019-04-24 07:38:59 +0200

    Move to the options model

---
+++ src/ServiceComposer.AspNetCore.Mvc/ViewModelCompositionMvcOptions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace ServiceComposer.AspNetCore.Mvc
+{
+    public class ViewModelCompositionMvcOptions
+    {
+        private IServiceCollection services;
+
+        internal ViewModelCompositionMvcOptions(IServiceCollection services, bool isAssemblyScanningDisabled)
+        {
+            this.services = services;
+            IsAssemblyScanningDisabled = isAssemblyScanningDisabled;
+        }
+
+        public bool IsAssemblyScanningDisabled { get; private set; }
+
+        public void RegisterResultHandler<T>()
+            where T : IHandleResult
+        {
+            RegisterResultHandler(typeof(T));
+        }
+
+        public void RegisterResultHandler(Type type)
+        {
+            services.AddSingleton(typeof(IHandleResult), type);
+        }
+    }
+}
---
+++ src/ServiceComposer.AspNetCore.Mvc/ViewModelCompositionOptionsExtensions.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace ServiceComposer.AspNetCore.Mvc
+{
+    public static class ViewModelCompositionOptionsExtensions
+    {
+        public static void AddMvcSupport(this ViewModelCompositionOptions compositionOptions, Action<ViewModelCompositionMvcOptions> config)
+        {
+            var mvcCompositionOptions = new ViewModelCompositionMvcOptions(
+                    compositionOptions.Services, 
+                    compositionOptions.IsAssemblyScanningDisabled);
+
+            config?.Invoke(mvcCompositionOptions);
+            if (!compositionOptions.IsAssemblyScanningDisabled)
+            {
+                var fileNames = Directory.GetFiles(AppContext.BaseDirectory);
+                var types = new List<Type>();
+                foreach (var fileName in fileNames)
+                {
+                    types.AddRange(Assembly.LoadFrom(fileName).GetTypesFromAssembly());
+                }
+
+                var platformAssembliesString = (string)AppDomain.CurrentDomain.GetData("TRUSTED_PLATFORM_ASSEMBLIES");
+                if (platformAssembliesString != null)
+                {
+                    var platformAssemblies = platformAssembliesString.Split(Path.PathSeparator);
+                    foreach (var platformAssembly in platformAssemblies)
+                    {
+                        types.AddRange(Assembly.LoadFrom(platformAssembly).GetTypesFromAssembly());
+                    }
+                }
+
+                foreach (var type in types)
+                {
+                    mvcCompositionOptions.RegisterResultHandler(type);
+                }
+            }
+
+            //TODO: throw if MvcOptions is not defined, it means AddMvc has not been yet called.
+            compositionOptions.Services.Configure<MvcOptions>(options => 
+            {
+                options.Filters.Add(typeof(CompositionActionFilter));
+            });
+        }
+
+        static IEnumerable<Type> GetTypesFromAssembly(this Assembly assembly)
+        {
+            var types = assembly.GetTypes()
+                .Where(t =>
+                {
+                    var typeInfo = t.GetTypeInfo();
+                    return !typeInfo.IsInterface
+                        && !typeInfo.IsAbstract
+                        && typeof(IHandleResult).IsAssignableFrom(t);
+                });
+
+            return types;
+        }
+    }
+}
