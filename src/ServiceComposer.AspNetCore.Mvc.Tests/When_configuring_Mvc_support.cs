74b6713a9406c212b5a410518c094421dd6ed5a1
Mauro Servienti <mauro.servienti@gmail.com>
2019-04-29 21:32:58 +0200

    Make sure composition filter is added to MVC

--- src/ServiceComposer.AspNetCore.Mvc.Tests/When_configuring_Mvc_support.cs
+++ src/ServiceComposer.AspNetCore.Mvc.Tests/When_configuring_Mvc_support.cs
@@ -1,7 +1,11 @@
-using ServiceComposer.AspNetCore.Gateway;
-using Xunit;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
 using ServiceComposer.AspNetCore.Testing;
 using System;
+using System.Linq;
+using Xunit;
 
 namespace ServiceComposer.AspNetCore.Mvc.Tests
 {
@@ -10,14 +14,13 @@ namespace ServiceComposer.AspNetCore.Mvc.Tests
         [Fact]
         public void Should_fail_if_no_mvc_is_configured()
         {
-            Assert.Throws<InvalidOperationException>(() => 
+            Assert.Throws<InvalidOperationException>(() =>
             {
                 // Arrange
                 var factory = new SelfContainedWebApplicationFactoryWithWebHost<When_configuring_Mvc_support>
                 (
                     configureServices: services =>
                     {
-                        //services.AddMvc();
                         services.AddViewModelComposition(options =>
                         {
                             options.AddMvcSupport();
@@ -25,11 +28,50 @@ namespace ServiceComposer.AspNetCore.Mvc.Tests
                     },
                     configure: app =>
                     {
-                        app.RunCompositionGatewayWithDefaultRoutes();
+                        app.UseMvc();
                     }
                 );
+
+                // Act
                 factory.CreateClient();
             });
         }
+
+        [Fact]
+        public void Make_sure_filter_is_added_to_mvc()
+        {
+            MvcOptions mvcOptions = null;
+
+            // Arrange
+            var factory = new SelfContainedWebApplicationFactoryWithWebHost<When_configuring_Mvc_support>
+            (
+                configureServices: services =>
+                {
+                    services.AddMvc(options => mvcOptions = options);
+                    services.AddViewModelComposition(options =>
+                    {
+                        options.AddMvcSupport();
+                    });
+                },
+                configure: app =>
+                {
+                    app.UseMvc();
+                }
+            );
+
+            // Act
+            factory.CreateClient();
+
+            // Assert
+            var registeredFilter = mvcOptions
+                .Filters
+                .SingleOrDefault(f =>
+                {
+                    return f is TypeFilterAttribute tfa 
+                        && tfa.ImplementationType == typeof(CompositionActionFilter);
+                });
+
+            Assert.NotNull(registeredFilter);
+        } 
     }
 }
